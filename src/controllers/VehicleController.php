fda6c7d9035c4b15693f15aa7bf4eb22cba9b49d
pSus365 <suotr12@gmail.com>
2026-01-14 12:18:25 +0100

    sorting vehicles by status

--- src/controllers/VehicleController.php
+++ src/controllers/VehicleController.php
@@ -16,9 +16,21 @@ class VehicleController extends AppController
     public function index()
     {
         $vehicles = $this->vehicleRepository->getVehicles();
+        $filter = $_GET['filter'] ?? 'all';
+
+        if ($filter === 'active') {
+            $vehicles = array_filter($vehicles, function ($vehicle) {
+                return $vehicle['status'] === 'wolny';
+            });
+        } elseif ($filter === 'inactive') {
+            $vehicles = array_filter($vehicles, function ($vehicle) {
+                return $vehicle['status'] !== 'wolny';
+            });
+        }
 
         $this->render('vehicles', [
-            'vehicles' => $vehicles
+            'vehicles' => $vehicles,
+            'currentFilter' => $filter
         ]);
     }
 }
